391b06b25647ae5fb4ccba0ef66f01be3abcb719
Liuyunda <man021436@163.com>
2023-11-22 14:38:58 +0800

    Initial Commit

---
+++ MeetingCheckin/MeetingCheckinApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingCheckinApp.swift
+//  MeetingCheckin
+//
+//  Created by 刘云达 on 2023/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MeetingCheckinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
